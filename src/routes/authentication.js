90c7da573320491ca417a16927d26ce704fccf33
Jônatas Vargatt <vargattjh@gmail.com>
2020-05-30 21:54:00 -0300

    'final'

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,31 +2,37 @@ const express = require('express');
 const router = express.Router();
 
 const passport = require('passport');
+const { isLoggedin, isNotLogedIn } = require('../lib/auth')
 
-router.get('/signin', (req, res) => {
+router.get('/signin', isNotLogedIn, (req, res) => {
   res.render('auth/signin');
 })
 
-router.post('/signin', (req, res, next) => {
+router.post('/signin', isNotLogedIn, (req, res, next) => {
   passport.authenticate('local.signin', {
     successRedirect: '/profile',
     failureRedirect: '/signin',
     failureFlash: true
-  })(req,res, next);
+  })(req, res, next);
 })
 
-router.get('/signup', (req, res) => {
+router.get('/signup', isNotLogedIn, (req, res) => {
   res.render('auth/signup');
 });
 
-router.post('/signup', passport.authenticate('local.signup', {
+router.post('/signup', isNotLogedIn, passport.authenticate('local.signup', {
   successRedirect: '/profile',
   failureRedirect: '/signup',
   failuresFlash: true,
 }));
 
-router.get('/profile', (req, res) => {
-  res.send('this is yout profile')
+router.get('/profile', isLoggedin, (req, res) => {
+  res.render('profile');
 })
+
+router.get('/logout', isLoggedin, (req, res) => {
+  req.logout();
+  res.redirect('signin');
+});
 
 module.exports = router;
--- src/routes/links.js
+++ src/routes/links.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
-
+const {isLoggedin} = require('../lib/auth');
 //Adicionando link
-router.get('/add', (req, res) => {
+router.get('/add', isLoggedin,  (req, res) => {
   res.render('links/add');
 })
 
-router.post('/add', async (req, res) => {
+router.post('/add', isLoggedin, async (req, res) => {
   const { title, url, description } = req.body;
   const newLink = {
     title,
     url,
     description,
+    user_id: req.user.id,
   }
   await pool.query('INSERT INTO links set ?', [newLink]);
   req.flash('success', 'Link saved success fully'); 
@@ -20,14 +21,14 @@ router.post('/add', async (req, res) => {
 });
 
 //Mostrando links
-router.get('/', async (req, res) => {
-  const links = await pool.query('SELECT * FROM links');
+router.get('/', isLoggedin, async (req, res) => {
+  const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
   res.render('links/list', { links: links });
   console.log(links);
 });
 
 //Deletando links
-router.get('/delete/:id', async (req, res) => {
+router.get('/delete/:id',isLoggedin, async (req, res) => {
   const { id } = req.params;
   await pool.query('DELETE FROM links WHERE ID = ?', [id]);
   req.flash('success', 'Link Removed')
@@ -35,14 +36,14 @@ router.get('/delete/:id', async (req, res) => {
 });
 
 //Editando Links
-router.get('/edit/:id', async (req, res) => {
+router.get('/edit/:id',isLoggedin, async (req, res) => {
   const { id } = req.params;
   const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
   console.log(links)
   res.render('links/edit', { links: links[0] })
 });
 
-router.post('/edit/:id', async (req, res) => {
+router.post('/edit/:id', isLoggedin, async (req, res) => {
   const { id } = req.params;
   const {title, description, url} = req.body;
   const newLink = {
